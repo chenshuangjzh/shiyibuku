af1c0ce480752bfc9b33885284f104c7fc2c6946
jiangzhenghui <970429796@qq.com>
2017-03-22 22:08:03 +0800

    修改导航栏相关的错误连接，拦截了500错误页面

--- src/main/webapp/static/js/nav.js
+++ src/main/webapp/static/js/nav.js
@@ -2,7 +2,9 @@ $(function(){
     $("#times").click(function(){
         var currentMilliseconds = new Date().getMilliseconds();
         Cookies.set("requestBegin",currentMilliseconds,{expires : 1});
-        window.location.href = "times/running";
+        var locationArray = window.location.href.split("/");
+        var targetLocation = locationArray[0] + "//" + locationArray[2] + "/" + locationArray[3] + "/";
+        window.location.href = targetLocation + "times/running";
     });
 
     $(".dropdown-toggle").dropdown();
